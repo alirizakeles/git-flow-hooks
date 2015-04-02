6e72096075c138ad27fcc72a249bbf3eecb1f11a
Jason Ivers <jasonivers@gmail.com>
2015-04-02 13:53:58 -0700

    Fix coloring in most places, add posting to slack on feature/hotfix/release finish, remove automatic versioning

--- config.sh
+++ config.sh
@@ -1,3 +1,6 @@
 # pre-commit
 #pre_commit_modules="jshint.sh phplint.sh prevent-master-commit.sh prevent-merge-marker-commits.sh"
 pre_commit_modules="prevent-master-commit.sh prevent-merge-marker-commits.sh"
+post_flow_feature_finish_modules="post-slack.sh"
+post_flow_hotfix_finish_modules="post-slack.sh"
+post_flow_release_finish_modules="post-slack.sh"
